dd966014062bdff66b1c4f93346e572e5fb6fe66
Dhanuka <2019cs063@stu.ucsc.cmb.ac.lk>
2024-04-08 17:45:31 +0530

    create componenet & set routing

--- JWT_LOGIN_ANGULAR/src/app/app.module.ts
+++ JWT_LOGIN_ANGULAR/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+
+//DEFINE ROUTE
+const routes:Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '',redirectTo:'login', pathMatch: 'full'},
+  {path: '',component:LayoutComponent,children:[{path:'dashboard',component:DashboardComponent}]},
+  {path:'**',component:LoginComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
